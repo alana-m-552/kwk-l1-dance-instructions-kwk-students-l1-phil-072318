f5b46e2172341bffd1d3a3459dcfd33f693cd383
alana-m-552 <alana.mackey0552@gmail.com>
2018-07-24 18:59:17 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -18,8 +18,9 @@ end
 
 def bob_the_reins
   puts "Holding your arms out in front of you, straight and at chest level"
-  puts "cross your right wrist over your left and hold them together"
+  puts "Cross your right wrist over your left and hold them together"
   puts "Lift your arms up and down in a loose bouncing movement, in time with the beat of the song. do this seven more times."
+  puts "bob it out"
 end 
 
 def lasso 
