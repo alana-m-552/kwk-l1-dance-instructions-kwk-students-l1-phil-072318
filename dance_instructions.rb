56ea0cc3bff552bdf67328eb6791d8dce1c1c20b
alana-m-552 <alana.mackey0552@gmail.com>
2018-07-24 18:51:33 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -1,2 +1,44 @@
 
 # Code your methods below
+def starting_stance
+  puts "plant legs far apart, bend knees slightly and keep posture loose"
+end 
+def base_footwork
+  puts "lift right foot"
+  puts "return right foot to the ground"
+  puts "finishing with a small skip-step backward"
+end 
+
+def skip_step_(starting_foot)
+  puts "lower #{staring_foot} foot to the ground"
+  puts "Bounce #{staring_foot} foot back up slightly, kicking it a few inches back"
+  puts "briefly raise the opp foot then #{starting_foot} blah blah."
+end 
+
+def bob_the_reins
+  puts "Holding your arms out in front of you, straight and at chest level"
+  puts "cross your right wrist over your left and hold them together"
+  puts "Lift your arms up and down in a loose bouncing movement, in time with the beat of the song. do this seven more times."
+end 
+
+def lasso 
+  puts "start arm movement by holding left arm so the back of your wrist is near your chin, pointing your left elbow straight left."
+  puts " lift right arm up and point right elbow diagonally to the right" 
+  puts "Raise right forearm so that it points straight up, and whip it in small circles to the beat of the song, as if you were a cowboy holding a rope lasso. This movement is repeated eight times as well." 
+  puts "Do this seven more times"
+end 
+
+def bust_a_move
+  puts "be cool"
+  puts "be snazzy"
+end 
+
+def Gangnum_Style 
+  starting_stance
+  base_footwork
+  skip_step_(right)
+  bob_the_reins
+  skip_step_(left)
+  lasso
+  bust_a_move
+end 
---
+++ practice.rb
@@ -0,0 +1,4 @@
+def hello(your_name,my_name)
+  puts "Hello #{your_name}! I'm #{my_name}"
+end 
+hello("Smoney","Alana")
