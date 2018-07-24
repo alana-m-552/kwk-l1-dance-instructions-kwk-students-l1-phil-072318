7ed6f02610e2a7bc1b897c33868edcfdada9bdac
alana-m-552 <alana.mackey0552@gmail.com>
2018-07-24 18:56:40 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -10,9 +10,9 @@ def base_footwork
   puts "finishing with a small skip-step backward"
 end 
 
-def skip_step(staring_foot)
-  puts "Lower #{staring_foot} to the ground"
-  puts "Bounce #{staring_foot} back up slightly, kicking it a few inches back"
+def skip_step(starting_foot)
+  puts "Lower #{starting_foot} to the ground"
+  puts "Bounce #{starting_foot} back up slightly, kicking it a few inches back"
   puts "Briefly raise the opposite foot, then the #{starting_foot} twice more!"
 end 
 
